ebebfd1ff8a9d07b0a42b2226929de6981200d07
Andre Untiedt <untiedt2005@yahoo.com>
2012-10-22 10:25:39 -0700

    JSON using Jackson

---
+++ tools/src/main/java/com/dredom/json/Category.java
@@ -0,0 +1,30 @@
+package com.dredom.json;
+
+import java.util.List;
+
+public class Category {
+
+	private Integer id;
+	private String name;
+	private List<SubCat> subCategories;
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<SubCat> getSubCategories() {
+		return subCategories;
+	}
+	public void setSubCategories(List<SubCat> subCategories) {
+		this.subCategories = subCategories;
+	}
+
+}
---
+++ tools/src/main/java/com/dredom/json/SubCat.java
@@ -0,0 +1,28 @@
+package com.dredom.json;
+
+public class SubCat {
+
+	private Integer id;
+	private String name;
+	private Integer categoryId;
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+
+}
