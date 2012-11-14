14d9f93134943a49d5d724e6ad98a7182cf30831
Andre Untiedt <untiedt2005@yahoo.com>
2012-11-13 16:01:52 -0800

    More annotations research

--- samples/src/main/java/com/dredom/annotation/AnnoteStuff.java
+++ samples/src/main/java/com/dredom/annotation/AnnoteStuff.java
@@ -1,11 +1,14 @@
 package com.dredom.annotation;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Documented
-@Retention(RetentionPolicy.SOURCE)
-@interface AnnoteStuff {
-	String stuffy();
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AnnoteStuff {
+	String stuffy() default "Schtuff";
 }
--- samples/src/main/java/com/dredom/annotation/UseAnnoteStuff.java
+++ samples/src/main/java/com/dredom/annotation/UseAnnoteStuff.java
@@ -1,16 +1,41 @@
 package com.dredom.annotation;
 
-import java.lang.reflect.AnnotatedElement;
+import static java.lang.System.out;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 
 public class UseAnnoteStuff {
+
+	public String stuff;
 
 	/**
 	 * Do it.
 	 */
 	@AnnoteStuff(stuffy = "Geronimo")
 	public void doit() {
-		// TODO
-		AnnotatedElement target = getClass();
+		System.out.println("Hello stuff");
+
+		Annotation[] annotes = getClass().getAnnotations();
+
+		if (annotes.length > 0) {
+			out.print("Class: " + getClass().getName() + "[");
+			for (Annotation annote : annotes) {
+				System.out.print(annote.toString());
+			}
+			out.println("]");
+		}
+		for (Method method : getClass().getMethods() ) {
+			if (method.getAnnotations().length > 0) {
+				out.format(" Method %s [", method.getName());
+				Annotation[] mannotes = method.getAnnotations();
+				for (Annotation mannote : mannotes) {
+					System.out.println(mannote.toString());
+					out.format("%s, ", mannote);
+				}
+				out.println("]");
+			}
+		}
 
 	}
 }
