b2c4bea3f9e1848f58693b8a9279112faf13b4b6
Andre Untiedt <untiedt2005@yahoo.com>
2013-09-30 20:05:47 -0700

    JSF form example, mvn jetty:run-war

--- jsfsample/src/main/java/com/dredom/WelcomeBean.java
+++ jsfsample/src/main/java/com/dredom/WelcomeBean.java
@@ -5,13 +5,23 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+/**
+ * Defaults to "welcomeBean" for JSF.
+ */
 @ManagedBean
 @SessionScoped
 public class WelcomeBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private String message = "I am alive!";
+	private String name;
 	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	public WelcomeBean() {
 		System.out.println("WelcomeBean instantiated");
 	}
