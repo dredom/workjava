31d571e42ff1201e8a8bae249ea9d81a746af771
Andre Untiedt <untiedt2005@yahoo.com>
2014-01-21 19:35:48 -0800

    Heap sort

---
+++ algorithm/src/main/java/com/dredom/sort/HeapSort.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+package com.dredom.sort;
+
+/**
+ * Builds a heap from the input. So heap root will be highest value.
+ * Traverse heap breadth-first building array.
+ * Iteratively...
+ * - Swap heap root to end of array, length - 1
+ * - Swap heap max node with last, remove last.
+ * - Re-heapify to make heap root the max again.
+ *
+ */
+public class HeapSort {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
