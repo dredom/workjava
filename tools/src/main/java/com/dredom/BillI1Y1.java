bed8d2a481b8c9fc793fdcc9e5a37db502d455f6
Andre Untiedt <untiedt2005@yahoo.com>
2014-05-09 09:57:16 -0700

    Create BillI1Y1.java

---
+++ tools/src/main/java/com/dredom/BillI1Y1.java
@@ -0,0 +1,44 @@
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class BillI1Y1 {
+
+    @XmlElement(name="Z_PRIMARY")
+    private String zPrimary;
+    @XmlElement(name="Z_DOC_DATE")
+    private String zDocDate;
+    @XmlElement(name="Z_DOC_ID")
+    private String zDocId;
+    @XmlElement(name="CustName")
+    private String custName;
+    @XmlElement(name="ServiceAdd")
+    private String serviceAddress;
+    @XmlElement(name="ServiceAdd1")
+    private String serviceAddress1;
+    @XmlElement(name="ZipCode")
+    private String zipCode;
+    @XmlElement(name="Usage")
+    private String usage;
+    @XmlElement(name="DueDate")
+    private String dueDate;
+    @XmlElement(name="CurrChgs")
+    private String currCharges;
+    @XmlElement(name="Rebill")
+    private String rebill;
+    @XmlElement(name="OMR_38")
+    private String omr38;
+    @XmlElement(name="Cancelled")
+    private String cancelled;
+    @XmlElement(name="PeriodStart")
+    private String periodStart;
+    @XmlElement(name="PeriodEnd")
+    private String periodEnd;
+    @XmlElement(name="PaymentAmt")
+    private String paymentAmount;
+    @XmlElement(name="ReadingsPrev")
+    private String readingsPrev;
+    @XmlElement(name="ReadingsPres")
+    private String readingsPres;
+}
+    
+
+    
