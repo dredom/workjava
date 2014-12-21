6853bd0701f1f32cb957bbe4458722b8e7ebcca3
Andre Untiedt <untiedt2005@yahoo.com>
2014-12-20 16:13:53 -0800

    A more efficient ReverseString using char[]

--- algorithm/src/main/java/com/dredom/ReverseString.java
+++ algorithm/src/main/java/com/dredom/ReverseString.java
@@ -1,16 +1,23 @@
 package com.dredom;
 
+/**
+ * String is immutable, based on final char[].
+ * charAt(i) will return the primitive value.
+ *
+ * @author andre
+ *
+ */
 public class ReverseString {
     static String INPUT = "ABCD";
 
     public static void main(String[] args) {
         String input = INPUT;
-        char[] chars = input.toCharArray();
         int len = input.length();
-        StringBuilder buf = new StringBuilder();
-        for (int i = len - 1; i >= 0; i--) {
-            buf.append(chars[i]);
+        char[] out = new char[len];
+        for (int j = 0, k = len - 1; j < len; j++, k--) {
+        	out[k] = input.charAt(j);
         }
-        System.out.println(buf);
+
+        System.out.println(out);
     }
 }
