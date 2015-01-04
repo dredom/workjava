6800bb0b02d014088977433253d14dde2fbe300d
Andre Untiedt <untiedt2005@yahoo.com>
2015-01-03 17:05:55 -0800

    Heap sort example

--- algorithm/src/main/java/com/dredom/sort/HeapSort.java
+++ algorithm/src/main/java/com/dredom/sort/HeapSort.java
@@ -3,22 +3,106 @@
  */
 package com.dredom.sort;
 
+import java.util.Arrays;
+
 /**
- * Builds a heap from the input. So heap root will be highest value.
- * Traverse heap breadth-first building array.
- * Iteratively...
+ * Builds a heap from the input. So heap root (1st) will be highest value.
+ * Traverse heap breadth-first building array. Iteratively...
  * - Swap heap root to end of array, length - 1
  * - Swap heap max node with last, remove last.
  * - Re-heapify to make heap root the max again.
- *
+ * <p>
+ * Heapsort is a comparison-based sorting algorithm to create a sorted array (or list).
+ * Although somewhat slower in practice on most machines than a well-implemented quicksort,
+ * it has the advantage of a more favorable worst-case O(n log n) runtime.
+ * Heapsort is an in-place algorithm, but it is not a stable sort (does not maintain the relative order of records with equal keys).
+ * <p>
+ * Worst case performance : O(n log n)
+ * Best case performance : O(n log n)
+ * Average case performance : O(n log n)
  */
 public class HeapSort {
 
+	private static int N;
+
+	/* Sort Function */
+	public static void sort(int arr[]) {
+
+		N = arr.length - 1;
+
+		// Put max value in 1st position
+		heapify(arr);
+
+		// Progress through values, right to left
+		for (int i = N; i > 0; i--) {
+
+			// 1st position is max; swap it to end.
+			swap(arr, 0, i);
+
+			N = N - 1;
+			maxheap(arr, 0);
+
+		}
+
+	}
+
+	/* Function to build a heap */
+	public static void heapify(int arr[]) {
+
+		// Start from middle; progress to left.
+		for (int i = N / 2; i >= 0; i--)
+			maxheap(arr, i);
+
+	}
+
 	/**
-	 * @param args
+	 * Function to swap largest element in heap into first position.
 	 */
+	public static void maxheap(int arr[], int i) {
+
+		int left = 2 * i;
+		int right = 2 * i + 1;
+		int max = i;
+
+		if (left <= N && arr[left] > arr[i])
+			max = left;
+
+		if (right <= N && arr[right] > arr[max])
+			max = right;
+
+		if (max != i) {
+
+			swap(arr, i, max);
+			maxheap(arr, max);
+
+		}
+
+	}
+
+	/* swap two numbers in an array */
+	public static void swap(int arr[], int i, int j) {
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+
+	/* Main method */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		/* Make array of n elements */
+
+		int arr[] = { 2, 5, 1, 3, 7 };
+		System.out.println(Arrays.toString(arr));
+		int n = arr.length;
+
+		/* Call method sort */
+
+		sort(arr);
+
+		/* Print sorted Array */
+		System.out.println("\nElements after sorting ");
+		System.out.println(Arrays.toString(arr));
+		System.out.println();
 
 	}
 
