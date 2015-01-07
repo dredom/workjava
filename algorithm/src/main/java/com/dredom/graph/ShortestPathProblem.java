6dbd4f6365759fa7b95c1dea66cbff0d9f480fbb
Andre Untiedt <untiedt2005@yahoo.com>
2015-01-06 21:20:54 -0800

    Shortest path problem - BFS

---
+++ algorithm/src/main/java/com/dredom/graph/ShortestPathProblem.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+package com.dredom.graph;
+
+/**
+ * Shortest path between two points.
+ * Given start and target.
+ * <pre>
+ *   A
+ *   |
+ *   B--C--D
+ *   |     |
+ *   E-----Z
+ * </pre>  Shorted path is A-B-E-Z = 4.
+ *
+ * <p>Variation:
+ * Each edge has a weight, as in destinations on a map.
+ *
+ * @author andre
+ *
+ */
+public class ShortestPathProblem {
+
+}
