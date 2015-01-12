475b33c0e7bb9519ff99e7d2bdbe6742c5935d65
Andre Untiedt <untiedt2005@yahoo.com>
2015-01-11 16:28:35 -0800

    Optimize; fix bug

--- algorithm/src/main/java/com/dredom/sort/QuickSort.java
+++ algorithm/src/main/java/com/dredom/sort/QuickSort.java
@@ -3,11 +3,12 @@ package com.dredom.sort;
 import static java.lang.System.out;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * QuickSort is a divide-and-conquer algorithm.
- * 
+ *
  * function quicksort('array')
  * 		if length('array') ≤ 1
  *      	return 'array'  // an array of zero or one elements is already sorted
@@ -17,14 +18,16 @@ import java.util.List;
  *      	if 'x' ≤ 'pivot' then append 'x' to 'less'
  *          else append 'x' to 'greater'
  *      return concatenate(quicksort('less'), list('pivot'), quicksort('greater')) // two recursive calls
- *      
+ *
  *
  */
 public class QuickSort {
 
     static Integer[] array = { 21, 3, 1, 11, 5, 2, 7 };
+//    static Integer[] array = { 2, 3, 4, 5, 7, 8, 9, 10 };
 //    static Integer[] array = { 21, 3, 1};
-    static Integer[] ia = {};
+
+    static int iterations;
 
     /**
      * @param args
@@ -32,31 +35,45 @@ public class QuickSort {
     public static void main(String[] args) {
         print(array);
         long start = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++) {
 
-            sort(array);
-        }
-        out.println(System.currentTimeMillis() - start + "ms");
+        Integer[] sorted = sort(array);
+
+        print(sorted);
+        out.println(System.currentTimeMillis() - start + "ms" + " in " + iterations + " iterations");
     }
 
     static Integer[] sort(Integer[] input) {
         if (input.length <= 1) {
             return input;
         }
+        iterations++;
         List<Integer> left = new ArrayList<Integer>();
         List<Integer> right = new ArrayList<Integer>();
         int mi = input.length / 2;
-        Integer middle = input[mi];
+        int middle = input[mi];
+        boolean isSorted = true;
         for (int i = 0; i < input.length; i++) {
             if (i != mi) {
-                if (input[i] <= middle) {
+                if (input[i].intValue() <= middle) {
                     left.add(input[i]);
+                    if (i > mi) {	// optimize
+                    	isSorted = false;
+                    }
                 } else {
                     right.add(input[i]);
+                    if (i < mi) {	// optimize
+                    	isSorted = false;
+                    }
                 }
             }
         }
-        return concatenate(sort(left.toArray(ia)), middle, sort(right.toArray(ia)));
+        if (isSorted) {	// optimize
+        	return input;
+        }
+        return concatenate(
+        		sort(left.toArray(new Integer[left.size()])),
+        		middle,
+        		sort(right.toArray(new Integer[right.size()])));
     }
 
     static Integer[] concatenate(Integer[] left, Integer middle, Integer[] right) {
@@ -68,11 +85,7 @@ public class QuickSort {
     }
 
     static void print(Integer[] arr) {
-        out.print('[');
-        for (int i = 0; i < arr.length; i++) {
-            out.printf(" %d,", arr[i]);
-        }
-        out.println(']');
+        out.println(Arrays.deepToString(arr));
     }
 
 }
