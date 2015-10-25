c7cde82edf9099c41be4c960093edfc04f94e0a3
Andre Untiedt mac <untiedt2005@yahoo.com>
2015-10-24 17:39:39 -0700

    Stream.Builder

--- basics/src/main/java/com/dredom/stream/StreamStuff.java
+++ basics/src/main/java/com/dredom/stream/StreamStuff.java
@@ -10,6 +10,7 @@ public class StreamStuff {
     public static void main(String[] args) {
         StreamStuff instance = new StreamStuff();
         instance.basicTest();
+        instance.builderTest();
     }
 
     private void basicTest() {
@@ -18,6 +19,22 @@ public class StreamStuff {
         });
 
         Stream<String> stream = list.stream();
+        Consumer<? super String> consumer = item -> System.out.println(item);
+        stream.forEach(consumer);
+
+//        long count = stream.count();  // failss
+    }
+
+    private void builderTest() {
+        final String[] items = new String[] {
+                "One", "Two", "Three"
+        };
+
+        Stream.Builder<String> bldr = Stream.builder();
+        for (String item : items) {
+            bldr.accept(item);
+        }
+        Stream<String> stream = bldr.build();
         Consumer<? super String> consumer = item -> System.out.println(item);
         stream.forEach(consumer);
     }
